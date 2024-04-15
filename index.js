ed533784ec1b0dbd87f88421466939df8c0529e1
BIPODGHOSH <bipodghosh0@gmail.com>
2024-04-15 14:08:28 +0530

    for testing purpose

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.RENDER_PORT || 3000;
 
 app.use("/staf", stafRoutes);
 app.use("/menu", menuRoutes);
